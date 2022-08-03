d77fd053277a2ac9ccff4590ba3334d9b4bd5420
RS-prog <110522157+RS-prog@users.noreply.github.com>
2022-08-03 12:21:18 +0530

    Add files via upload

---
+++ triangle.py
@@ -0,0 +1,13 @@
+print("Program for calculating the area of triangle")
+a=int(input ("Enter the base length of triangle: "))
+b=int(input ("Enter the height of the triangle: "))
+c=0.5
+
+area= (a*b*c)
+print("Area of Triangle=", area,"sq.cm")
+d=a%b
+e=a**b
+f=a//b
+print("Modulus-", d)
+print("Exponent-", e)
+print("Floor div-", f)
